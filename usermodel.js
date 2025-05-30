7f82fb9053ab669d647a4df65e63f4ece68c2a1d
Bazeel Bin Waris <98183874+Bazeel-Waris@users.noreply.github.com>
2025-05-30 12:03:37 +0500

    It is monog-crud app changes

---
+++ usermodel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mongopractice');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
